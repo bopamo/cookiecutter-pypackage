80fc00fb1c29ae1c8cb9af4e3c1964deaeb5ef73
Bopamo Osaisai <bopamo@gmail.com>
2019-09-11 14:48:27 -0700

    Updating repo info and adding pre and post gen hooks

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,13 +1,35 @@
 import re
 import sys
 
+PRETTY_HEADER = "*****************************"
+PRE_GEN_HOOK_MESSAGE = PRETTY_HEADER + " PRE GEN HOOK RUNNING " + PRETTY_HEADER
 
-MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
-module_name = '{{ cookiecutter.project_slug}}'
+def test_module_name():
+    MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 
-if not re.match(MODULE_REGEX, module_name):
-    print('ERROR: The project slug (%s) is not a valid Python module name. Please do not use a - and use _ instead' % module_name)
+    module_name = "{{ cookiecutter.project_slug}}"
 
-    #Exit to cancel project
-    sys.exit(1)
+    if not re.match(MODULE_REGEX, module_name):
+        print(
+            "ERROR: The project slug (%s) is not a valid Python module name. Please do not use a - and use _ instead" % module_name)
+
+        # Exit to cancel project
+        sys.exit(1)
+
+
+def test_village_name():
+    village_name = "{{ cookiecutter.village_name}}"
+
+    assert village_name == "Koluama 3"
+
+
+def run_pre_gen_project_hooks():
+    print(PRE_GEN_HOOK_MESSAGE)
+
+    test_module_name()
+    test_village_name()
+
+
+if __name__ == '__main__':
+    run_pre_gen_project_hooks()
